c258c92fb7477699c8de449143b76283990ffd22
Jonesyboy07 <145576901+Jonesyboy07@users.noreply.github.com>
2025-09-10 15:27:55 +0100

    Add INVITE_LINK to env and update usage in constants
    Added INVITE_LINK to .env.example for easier configuration and updated utils/constants.py to read INVITE_LINK from environment variables instead of constructing it in code.

--- utils/constants.py
+++ utils/constants.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DISCORD_CLIENT_ID = os.getenv("DISCORD_CLIENT_ID")
-INVITE_LINK = f"https://discord.com/api/oauth2/authorize?client_id={DISCORD_CLIENT_ID}&permissions=2147544129&integration_type=0&scope=bot+applications.commands"
+INVITE_LINK = os.getenv("INVITE_LINK", "N/A")
 
 
 DONATION_LINK = os.getenv("DONATION_LINK", "N/A")
